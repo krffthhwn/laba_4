41e588e625ee7ccfa4dd364191a2821813fc1eec
krffthhwn <fraineronq@gmail.com>
2023-10-05 23:39:08 +0300

    main

---
+++ laba 3.4/laba 3.4.cpp
@@ -0,0 +1,28 @@
+﻿// Lab_03_4.cpp
+// < Іванів Олександр Андрійович >
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 0.9
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x;
+	double y;
+	double R;
+
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+	// розгалуження в повній формі
+	if ((x * x + y * y <= R * R) && x < 0 && y < 0 || (x * x + y * y <= R * R) && ((y = pow(x - 1, 2) <= R)))
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
+
+	return 0;
+
+}
